e89b36e0ff4923456e14dbef46b63a0055d04313
isabel <acev0010@algonquinlive.com>
2024-07-07 20:32:56 -0400

    two instead of one

--- script.js
+++ script.js
@@ -48,6 +48,41 @@ function randomizeCharacter() {
     }
 }
 
+function randomizeTwoCharacters() {
+    // Get all character-small elements
+    let allSmallCharacters = document.querySelectorAll('.character-small');
+
+    // Remove flipped class from all character-small elements
+    allSmallCharacters.forEach(char => {
+        char.classList.remove('flipped');
+    });
+
+    let iterations = 10; // Number of iterations before final selection
+    let delay = 100; // Initial delay in milliseconds
+
+    for (let i = 0; i <= iterations; i++) {
+        setTimeout(() => {
+            let randomIndex1 = Math.floor(Math.random() * allSmallCharacters.length);
+            let randomIndex2;
+            do {
+                randomIndex2 = Math.floor(Math.random() * allSmallCharacters.length);
+            } while (randomIndex2 === randomIndex1);
+
+            // Remove flipped class from all character-small elements before adding it to the new ones
+            allSmallCharacters.forEach(char => {
+                char.classList.remove('flipped');
+            });
+
+            // Add flipped class to the randomly selected character-small elements
+            allSmallCharacters[randomIndex1].classList.add('flipped');
+            allSmallCharacters[randomIndex2].classList.add('flipped');
+
+            // Increase delay to slow down the animation
+            delay += 50;
+        }, i * delay);
+    }
+}
+
 function unflipAll() {
     let allLargeCharacters = document.querySelectorAll('.board#board1 .character');
     allLargeCharacters.forEach(char => {
